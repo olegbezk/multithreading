29b153f94bd8093850988405087941e259f4a51c
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-02-22 14:16:55 +0200

    synchronized keyword

---
+++ src/main/java/com/multithreading/demo5/App.java
@@ -0,0 +1,41 @@
+package com.multithreading.demo5;
+
+public class App {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.doWork();
+    }
+
+    private synchronized void increment() {
+        count++;
+    }
+
+    private void doWork() {
+        Thread one = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                increment();
+            }
+        });
+
+        Thread two = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                increment();
+            }
+        });
+
+        one.start();
+        two.start();
+
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Count is: " + count);
+    }
+}
