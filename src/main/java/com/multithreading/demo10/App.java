6de8c695a81748f66f8bba93fb71386e5a28e6cc
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@gmail.com>
2018-04-23 14:09:38 +0300

    using wait and notify in producer - consumer pattern

---
+++ src/main/java/com/multithreading/demo10/App.java
@@ -0,0 +1,34 @@
+package com.multithreading.demo10;
+
+/**
+ * Wait and notify
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Processor processor = new Processor();
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                processor.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                processor.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
