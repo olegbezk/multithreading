68da633e317c63aee650aaa68b829b9206718e10
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@gmail.com>
2018-06-13 14:23:17 +0300

    students library simulation

--- src/main/java/com/parrallel/computing/student/library/Book.java
+++ src/main/java/com/parrallel/computing/student/library/Book.java
@@ -1,6 +1,5 @@
 package com.parrallel.computing.student.library;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
